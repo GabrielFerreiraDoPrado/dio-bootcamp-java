4f7264a9fedb082a300ca65c9f3b946ee3e0e201
Gabriel Ferreira do Prado <thegaprado@gmail.com>
2022-12-12 19:09:46 -0300

    Abstraction and Encapsulation

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Curso curso1 = new Curso("Curso Java", "Descrição Curso Java", 8);
+		Curso curso2 = new Curso("Curso JavaScript", "Descrição Curso JavaScript", 10);
+		DateTimeFormatter  dtf =  DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		Mentoria mentoria1 = new Mentoria("Mentoria de Java", "Desccricao Mentoria de Java", LocalDate.parse("12/12/2022", dtf));
+		Mentoria mentoria2 = new Mentoria("Mentoria de JavaScript", "Desccricao Mentoria de JavaScript", LocalDate.parse("13/12/2022", dtf));
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		System.out.println(mentoria1);
+		System.out.println(mentoria2);
+
+	}
+}
